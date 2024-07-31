523f6aa1b8936f1224e730e3c8ef6f7092fa6f5c
levi <cgfeel@gmail.com>
2024-07-31 19:30:36 +0800

    upate

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      url: {{ url }}
+    </p>
+    <p>
+      <a @click="goToHome">replace go home</a>
+    </p>
+    <p>
+      <button @click="upHrefToHome">up href to home</button>
+    </p>
   </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const url = ref("");
+
+function goToHome() {
+  // this.$router.replace('/')
+  // 作为子应用这里是一个 `bug`
+  window.location.replace('http://localhost:8080/');
+}
+
+function upHrefToHome() {
+  window.location.href = "/";
+}
+
+onMounted(() => {
+  url.value = window.location.href;
+});
+</script>
