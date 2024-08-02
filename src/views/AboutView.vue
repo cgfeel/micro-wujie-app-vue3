9136bc5521942de253285ecfd39a71b57d2b1156
levi <cgfeel@gmail.com>
2024-08-02 19:52:26 +0800

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -20,11 +20,13 @@ const url = ref("");
 
 function goToHome() {
   // this.$router.replace('/')
-  // 作为子应用这里是一个 `bug`
+  // 作为子应用这里是一个 `bug`，会将链接替换成基座 http://localhost:3000/ 然后报错
   window.location.replace('http://localhost:8080/');
 }
 
 function upHrefToHome() {
+  // 在降级处理中这里是一个 `bug`，因为 `location` 并没有劫持，造成 `/` 指向基座首页，和上面 `replace` 一样的错误
+  console.log(window.location.href);
   window.location.href = "/";
 }
 
